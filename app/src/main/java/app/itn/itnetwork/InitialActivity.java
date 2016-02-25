340398888b06e1f98f3bac1654eb8e5f44597378
adam <jezek.adamek@gmail.com>
2016-02-25 21:35:26 +0100

    initial commit

---
+++ app/src/main/java/app/itn/itnetwork/InitialActivity.java
@@ -0,0 +1,47 @@
+package app.itn.itnetwork;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class InitialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial);
+
+        Button loginButton = (Button)findViewById(R.id.loginBtn);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+
+    public void login() {
+        new AlertDialog.Builder(InitialActivity.this)
+                .setTitle("Test dialog")
+                .setMessage("Showing text dialog")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        startActivity(new Intent(InitialActivity.this, MainApp.class));
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+
+}
